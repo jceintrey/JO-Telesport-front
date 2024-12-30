cb8cbef0709b1ac9b51285f2e99dbdd711f92011
Jérémie Ceintrey <jeremie.ceintrey@gmail.com>
2024-12-30 15:08:28 +0100

    docs: - ajout de jsdocs dans appComponent - génération typedoc
    feature: ajout méthode onDestroy dans appComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { take } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, take } from 'rxjs';
 import { OlympicService } from './core/services/olympic.service';
+import { NGXLogger } from 'ngx-logger';
 
 @Component({
   standalone: false,
@@ -8,10 +9,32 @@ import { OlympicService } from './core/services/olympic.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  constructor(private olympicService: OlympicService) {}
+export class AppComponent implements OnInit, OnDestroy {
+  private subscription: Subscription = new Subscription();
 
+  constructor(
+    private olympicService: OlympicService,
+    private logger: NGXLogger
+  ) {}
+
+  /**
+   * Initialise les données du composants
+   *
+   * - charge les données du service
+   * - s'abonne au service
+   *
+   */
   ngOnInit(): void {
     this.olympicService.loadInitialData().pipe(take(1)).subscribe();
+  }
+
+  /**
+   * Termine proprement lors de la destruction du composant.
+   *
+   * - Désabonne l'Observable.
+   */
+  ngOnDestroy(): void {
+    this.logger.debug('Desctruction du composant');
+    this.subscription.unsubscribe();
   }
 }
